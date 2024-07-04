c2f8cf07a7be9e3a91b2aa59218b6e8db322855d
Chintan Mahadik <chintanmahadik@gmail.com>
2024-07-04 21:05:30 +0530

    new practice problems

---
+++ com/chinz/usefulLinks.sh
@@ -0,0 +1,22 @@
+#Memory Management
+https://www.geeksforgeeks.org/java-memory-management/
+
+#String Pool Constants
+https://www.geeksforgeeks.org/string-constant-pool-in-java/
+
+#Collection Interface
+https://www.geeksforgeeks.org/collection-interface-in-java-with-examples/
+
+#Map Interface
+https://www.geeksforgeeks.org/map-interface-java-examples/
+
+#ClassLoaders in Java
+https://www.geeksforgeeks.org/classloader-in-java/
+
+#Main Method concepts
+https://www.geeksforgeeks.org/java-main-method-public-static-void-main-string-args/
+
+#Immutable classes
+https://www.geeksforgeeks.org/create-immutable-class-java/
+
+#Singleton classes
